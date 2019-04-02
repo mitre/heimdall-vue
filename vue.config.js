8cbaa9d830a6df28b488df6709d5c5f7bc4f755c
Robert Thew <rthew@mitre.org>
2019-04-02 16:17:33 -0400

    reverted packages

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,9 @@
 let HtmlWebpackPlugin = require('html-webpack-plugin');
 let HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
+  baseUrl: process.env.NODE_ENV === 'production' ? './' : '/',
   configureWebpack: {
-    plugins: [ 
+    plugins: [
       new HtmlWebpackPlugin({template: 'public/index.html',inlineSource: '.(js|css)$'}),
       new HtmlWebpackInlineSourcePlugin()
     ]
