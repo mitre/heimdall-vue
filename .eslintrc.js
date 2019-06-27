29a3f02852a437d1a550f2e38057647303b2ddc1
Malinowski <lmalinowski@mm249445-pc.mitre.org>
2019-06-27 12:45:30 -0400

    Fix #38 by updating eslint config to recognize vue plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,4 +11,14 @@ module.exports = {
     'no-console': 0,
   },
   plugins: ['vue'],
+  
+  extends: [
+    // add more generic rulesets here, such as:
+    // 'eslint:recommended',
+    'plugin:vue/recommended'
+  ],
+  rules: {
+    // override/add rules settings here, such as:
+    // 'vue/no-unused-vars': 'error'
+  }
 };
