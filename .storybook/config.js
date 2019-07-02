157387ccf72c3ba18b1eb3e65eaa7dd0469cc55f
lukemalinowski <40170735+lukemalinowski@users.noreply.github.com>
2019-07-01 21:43:20 -0400

    Storybook (#45)
    * Initialize Storybook
    
    * Add storybook configuration related to #44

---
+++ .storybook/config.js
@@ -0,0 +1,53 @@
+import { configure } from '@storybook/vue';
+
+import Vue from 'vue';
+import BootstrapVue from 'bootstrap-vue'
+
+Vue.use(BootstrapVue)
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+
+// Import your global components.
+
+// import ControlDetail from '../src/components/ControlDetail.vue';
+import AboutContent from '../src/components/AboutContent.vue';
+import CountCard from '../src/components/CountCard.vue';
+import SspContent from '../src/components/SspContent.vue';
+import ControlStatus from '../src/components/ControlStatus.vue';
+import FileReader from '../src/components/FileReader.vue';
+import ComplianceChart from '../src/components/ComplianceChart.vue';
+import ControlImpact from '../src/components/ControlImpact.vue';
+import DataTable from '../src/components/DataTable.vue';
+import NavHeader from '../src/components/NavHeader.vue';
+import Treemap from '../src/components/Treemap.vue';
+import Home from '../src/views/Home.vue';
+
+// Vue.component('ControlDetail',ControlDetail);
+Vue.component('AboutContent',AboutContent);
+Vue.component('CountCard',CountCard);
+Vue.component('SspContent',SspContent);
+Vue.component('ControlStatus',ControlStatus);
+Vue.component('FileReader',FileReader);
+Vue.component('ComplianceChart',ComplianceChart);
+Vue.component('ControlImpact',ControlImpact);
+Vue.component('DataTable',DataTable);
+Vue.component('NavHeader',NavHeader);
+Vue.component('Treemap',Treemap);
+Vue.component('Home',Home);
+
+
+// Register global components.
+// Vue.component('my-button', Mybutton);
+
+
+// automatically import all files ending in *.stories.js
+const req = require.context('../stories', true, /\.stories\.js$/);
+function loadStories() {
+  console.log("requirements!");
+  req.keys().forEach(filename => console.log(filename));
+  req.keys().forEach(filename => req(filename));
+}
+
+configure(loadStories, module);
---
+++ stories/index.stories.js
@@ -0,0 +1,39 @@
+/* eslint-disable react/react-in-jsx-scope */
+
+import { storiesOf } from "@storybook/vue";
+import { action } from "@storybook/addon-actions";
+import { linkTo } from "@storybook/addon-links";
+
+storiesOf("CountCard", module).add("Passing Count Card", () => ({
+  template:
+    '<CountCard title="Passed" explanation="(all tests passed)" fas_icon="check" color_variant="success"></CountCard>)'
+}));
+
+storiesOf("AboutContent", module).add("Test story 0", () => ({
+  template: '<AboutContent v-if="shouldShowAbout" />'
+}));
+storiesOf("ComplianceChart", module).add("Test story 1", () => ({
+  template: "<ComplianceChart/>"
+}));
+storiesOf("ControlImpact", module).add("Test story 3", () => ({
+  template: "<ControlImpact/>"
+}));
+storiesOf("ControlStatus", module).add("Test story 4", () => ({
+  template: "<ControlStatus/>"
+}));
+storiesOf("CountCard", module).add("Test story 5", () => ({
+  template:
+    '<CountCard title="Passed" explanation="(all tests passed)" fas_icon="check" color_variant="success"></CountCard> <CountCard title="Failed" explanation="(has tests that failed)" fas_icon="times" color_variant="danger"></CountCard> <CountCard title="Not Applicable" explanation="(exception for this system and/or absent component)" fas_icon="ban" color_variant="primary"></CountCard> <CountCard title="Not Reviewed" explanation="(can only be tested manually or disabled test)" fas_icon="exclamation-triangle" color_variant="warning"></CountCard> <CountCard title="Profile Error" explanation="(check profile run privileges or check with profile author)" fas_icon="exclamation-circle" color_variant="info"></CountCard>'
+}));
+storiesOf("DataTable", module).add("Test story 6", () => ({
+  template: '<DataTable :controls="filteredControls"></DataTable>'
+}));
+storiesOf("NavHeader", module).add("Test story 8", () => ({
+  template: "<NavHeader/>"
+}));
+storiesOf("SspContent", module).add("Test story 9", () => ({
+  template: '<SspContent v-else-if="shouldShowSSP" />'
+}));
+storiesOf("Treemap", module).add("Test story 10", () => ({
+  template: '<Treemap :key="resizeKey"/>'
+}));
