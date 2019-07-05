d38c7b2659fe04ca4b2373375ffcd62374ce34f5
Colin Doherty <oneofthree103@gmail.com>
2019-07-05 17:43:22 -0400

    Initial start of code highlighting - hard coded data - using standard card template component
    Signed-off-by: Colin Doherty <oneofthree103@gmail.com>

--- src/views/Page2.vue
+++ src/views/Page2.vue
@@ -1,3 +1,41 @@
 <template>
-	<h4>You are second page.</h4>
+	<vx-card title="Card Title" code-toggler codeLanguage="ruby">
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+		<template slot="codeContainer">
+control "V-38437" do
+  title "Automated file system mounting tools must not be enabled unless
+needed."
+  desc  "All filesystems that are required for the successful operation of the
+system should be explicitly listed in \"/etc/fstab\" by an administrator. New
+filesystems should not be arbitrarily introduced via the automounter.
+    The \"autofs\" daemon mounts and unmounts filesystems, such as user home
+directories shared via NFS, on demand. In addition, autofs can be used to
+handle removable media, and the default configuration provides the cdrom device
+as \"/misc/cd\". However, this method of providing access to removable media is
+not common, so autofs can almost always be disabled if NFS is not in use. Even
+if NFS is required, it is almost always possible to configure filesystem mounts
+statically by editing \"/etc/fstab\" rather than relying on the automounter.
+
+		</template>
+	</vx-card>
 </template>
+
+<script>
+import VxCard from '@/components/vx-card/VxCard.vue'
+
+export default {
+	components : {
+		VxCard
+	}
+}
+</script>
+
+<style>
+
+</style>
