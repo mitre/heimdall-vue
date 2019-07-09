04f466f27a11b06ea9e98c84284870f87b85542e
v1kramb <38473535+v1kramb@users.noreply.github.com>
2019-07-09 15:54:25 -0400

    Created CountCard component. (#63)
    Refs #61 #62
    Fixes #60
    
    Signed-off-by: Vikram Bala <vikrambala.va@gmail.com>

--- src/views/results.vue
+++ src/views/results.vue
@@ -3,60 +3,44 @@
     <!-- ROW 1 -->
     <div class="vx-row">
       <div class="vx-col w-full sm:w-1/2 md:w-1/2 lg:w-1/4 xl:w-1/4 mb-base">
-        <vx-card
-          title="Background Color"
-          title-color="#fff"
-          card-background="success"
-          content-color="#fff"
-        >
-          <p class="mb-3">
-            You can use
-            <strong>card-background</strong> prop to change background color of card. This prop supports hex, rgba, rgb and theme colors.
-          </p>
-          <p
-            class="mb-3"
-          >Oat cake powder sesame snaps. Chocolate bar dessert bonbon chocolate bar pudding apple pie muffin chocolate ice cream. I love bear claw I love.</p>
-        </vx-card>
+        <count-card
+          icon="CheckCircleIcon"
+          icon-right
+          statistic="138"
+          statisticTitle="Passed"
+          statisticSub="All tests passed."
+          background="success"
+        ></count-card>
       </div>
       <div class="vx-col w-full sm:w-1/2 md:w-1/2 lg:w-1/4 xl:w-1/4 mb-base">
-        <vx-card
-          title="Background Color"
-          title-color="#fff"
-          card-background="danger"
-          content-color="#fff"
-        >
-          <p class="mb-3">
-            You can use
-            <strong>card-background</strong> prop to change background color of card. This prop supports hex, rgba, rgb and theme colors.
-          </p>
-          <p
-            class="mb-3"
-          >Oat cake powder sesame snaps. Chocolate bar dessert bonbon chocolate bar pudding apple pie muffin chocolate ice cream. I love bear claw I love.</p>
-        </vx-card>
+        <count-card
+          icon="XCircleIcon"
+          icon-right
+          statistic="52"
+          statisticTitle="Errors"
+          statisticSub="Has tests that failed."
+          background="danger"
+        ></count-card>
       </div>
       <div class="vx-col w-full sm:w-1/2 md:w-1/2 lg:w-1/4 xl:w-1/4 mb-base">
-        <vx-card
-          title="Background Color"
-          title-color="#fff"
-          card-background="warning"
-          content-color="#fff"
-        >
-          <p class="mb-3">
-            You can use
-            <strong>card-background</strong> prop to change background color of card. This prop supports hex, rgba, rgb and theme colors.
-          </p>
-          <p
-            class="mb-3"
-          >Oat cake powder sesame snaps. Chocolate bar dessert bonbon chocolate bar pudding apple pie muffin chocolate ice cream. I love bear claw I love.</p>
-        </vx-card>
+        <count-card
+          icon="SlashIcon"
+          icon-right
+          statistic="25"
+          statisticTitle="Not Applicable"
+          statisticSub="System exception/absent component."
+          background="primary"
+        ></count-card>
       </div>
       <div class="vx-col w-full sm:w-1/2 md:w-1/2 lg:w-1/4 xl:w-1/4 mb-base">
-        <CountCard
-          title="Passed"
-          explanation="(all tests passed)"
-          fas_icon="check"
-          color_variant="success"
-        ></CountCard>
+        <count-card
+          icon="AlertTriangleIcon"
+          icon-right
+          statistic="12"
+          statisticTitle="Not Reviewed"
+          statisticSub="Manual testing required/disabled test."
+          background="warning"
+        ></count-card>
       </div>
     </div>
 
@@ -74,7 +58,7 @@
             <vue-apex-charts
               type="donut"
               height="330"
-			  class="mt-5"
+              class="mt-5"
               :options="analyticsData.sessionsByDeviceDonut.chartOptions"
               :series="analyticsData.sessionsByDeviceDonut.series"
             />
@@ -83,16 +67,26 @@
           <!-- CHART DATA -->
           <!-- CHART DATA -->
           <ul>
-              <li v-for="deviceData in analyticsData.sessionsByDeviceDonut.analyticsData" :key="deviceData.device" class="flex mb-3">
-                  <feather-icon :icon="deviceData.icon" :svgClasses="[`h-5 w-5 stroke-current text-${deviceData.color}`]"></feather-icon>
-                  <span class="ml-2 inline-block font-semibold">{{ deviceData.device }}</span>
-                  <span class="mx-2">-</span>
-                  <span class="mr-4">{{ deviceData.sessionsPercentgae }}%</span>
-                  <div class="ml-auto flex -mr-1">
-                  <span class="mr-1">{{ deviceData.comparedResultPercentage }}%</span>
-                  <feather-icon :icon=" deviceData.comparedResultPercentage < 0 ? 'ArrowDownIcon' : 'ArrowUpIcon'" :svgClasses="[deviceData.comparedResultPercentage < 0 ? 'text-danger' : 'text-success'  ,'stroke-current h-4 w-4 mb-1 mr-1']"></feather-icon>
-                  </div>
-              </li>
+            <li
+              v-for="deviceData in analyticsData.sessionsByDeviceDonut.analyticsData"
+              :key="deviceData.device"
+              class="flex mb-3"
+            >
+              <feather-icon
+                :icon="deviceData.icon"
+                :svgClasses="[`h-5 w-5 stroke-current text-${deviceData.color}`]"
+              ></feather-icon>
+              <span class="ml-2 inline-block font-semibold">{{ deviceData.device }}</span>
+              <span class="mx-2">-</span>
+              <span class="mr-4">{{ deviceData.sessionsPercentgae }}%</span>
+              <div class="ml-auto flex -mr-1">
+                <span class="mr-1">{{ deviceData.comparedResultPercentage }}%</span>
+                <feather-icon
+                  :icon=" deviceData.comparedResultPercentage < 0 ? 'ArrowDownIcon' : 'ArrowUpIcon'"
+                  :svgClasses="[deviceData.comparedResultPercentage < 0 ? 'text-danger' : 'text-success'  ,'stroke-current h-4 w-4 mb-1 mr-1']"
+                ></feather-icon>
+              </div>
+            </li>
           </ul>
         </vx-card>
       </div>
@@ -114,7 +108,7 @@ import VueApexCharts from "vue-apexcharts";
 import CountCard from "../components/CountCard.vue";
 // import StatisticsCardLine from '@/components/statistics-cards/StatisticsCardLine.vue'
 import analyticsData from "./ui-elements/card/analyticsData.js";
-import ChangeTimeDurationDropdown from '@/components/ChangeTimeDurationDropdown.vue'
+import ChangeTimeDurationDropdown from "@/components/ChangeTimeDurationDropdown.vue";
 
 export default {
   data() {
