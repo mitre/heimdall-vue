84724ed405e4ea624656ae4d7e3a9ad7c59197d7
Henry <jchenry@mm249849-pc.mitre.org>
2019-07-12 19:19:15 -0400

    Format

--- src/store/data_store.ts
+++ src/store/data_store.ts
@@ -4,35 +4,38 @@
 
 import { Module, VuexModule, Mutation, Action } from "vuex-module-decorators";
 import { Control, Profile, InspecOutput } from "inspecjs";
+import Store from "./store";
 
 @Module({
   namespaced: true,
+  name: "data",
+  store: Store,
 })
 class InspecDataModule extends VuexModule {
-   allControls: Control[] = [];
-   allProfiles: Profile[] = [];
-   allReports: InspecOutput[] = [];
+  allControls: Control[] = [];
+  allProfiles: Profile[] = [];
+  allReports: InspecOutput[] = [];
 
-  @Mutation 
-   addControl(newControl: Control) {
+  @Mutation
+  addControl(newControl: Control) {
     this.allControls.push(newControl);
   }
 
-  @Mutation 
-   addProfile(newProfile: Profile) {
+  @Mutation
+  addProfile(newProfile: Profile) {
     this.allProfiles.push(newProfile);
   }
 
-  @Mutation 
-   addReport(newReport: InspecOutput) {
+  @Mutation
+  addReport(newReport: InspecOutput) {
     this.allReports.push(newReport);
   }
 
   @Mutation
-   reset() {
-      this.allControls = [];
-      this.allProfiles = [];
-      this.allReports = [];
+  reset() {
+    this.allControls = [];
+    this.allProfiles = [];
+    this.allReports = [];
   }
 }
 
