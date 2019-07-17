4100860f63ad3fea7b541077fa891a4038f734af
Henry <jchenry@mm249849-pc.mitre.org>
2019-07-17 18:07:15 -0400

    API/ENH: Converted store/intake to track by file.
    Report and profile files are tracked separately.
    Each given a unique id for routing.

--- src/store/data_store.ts
+++ src/store/data_store.ts
@@ -1,54 +1,91 @@
 /**
- * Holds the actual data
+ * Tracks uploaded files, and their parsed contents
  */
 
 import { Module, VuexModule, Mutation, Action } from "vuex-module-decorators";
 import { Control, Profile, InspecOutput } from "inspecjs";
 import Store from "./store";
 
+/** Represents the minimum data to represent an uploaded file handle. */
+type File = {
+  /** 
+   * Unique identifier for this file. Used to encode which file is currently selected, etc. 
+   * 
+   * Note that in general one can assume that if a file A is loaded AFTER a file B, then
+   * A.unique_id > B.unique_id.
+   * Using this property, one might order files by order in which they were added.
+   */
+  unique_id: number,
+  /** The filename that this file was uploaded under. */
+  filename: string
+}
+/** Represents a file containing an Inspec Report output */
+type ReportFile = File & { report: InspecOutput };
+/** Represents a file containing an Inspec Profile (not run) */
+type ProfileFile = File & { profile: Profile };
+
+
 @Module({
   namespaced: true,
   name: "data",
 })
 class InspecDataModule extends VuexModule {
-  /** All controls that have been added */
-  allControls: Control[] = [];
-  /** All profiles that have been added */
-  allProfiles: Profile[] = [];
-  /** All reports that have been added */
-  allReports: InspecOutput[] = [];
+  /** State var containing all report files that have been added */
+  reportFiles: ReportFile[];
 
-  /**
-   * Adds a control to the store.
-   * @param newControl The control to add
+  /** State var containing all profile files that have been added */
+  profileFiles: ProfileFile[];
+
+  /** Return all of the files that we currently have. */
+  get allFiles(): (ReportFile | ProfileFile)[] {
+    let result: (ReportFile | ProfileFile)[] = [];
+    result.push(...this.reportFiles);
+    result.push(...this.profileFiles);
+    return result;
+  }
+
+  /** 
+   * Get all reports that have been added, regardless of originating file. 
+   * TODO: Deprecate this? The only "all" we still need is for the All Report view (?)
    */
-  @Mutation
-  addControl(newControl: Control) {
-    this.allControls.push(newControl);
+  get allReports(): InspecOutput[] {
+    return this.reportFiles.map(f => f.report);
+  }
+
+  /** 
+   * Get all profiles that have been added, regardless of originating file. 
+   * TODO: Deprecate this? The only "all" we still need is for the All Report view (?)
+   */
+  get allProfiles(): Profile[] {
+    let fileProfiles = this.profileFiles.map(f => f.profile);
+    let nestedProfiles = this.allReports.flatMap(r => r.profiles);
+    return fileProfiles.concat(nestedProfiles);
+  }
+
+  /** 
+   * Get all controls that have been added, regardless of originating file/profile 
+   * TODO: Deprecate this? The only "all" we still need is for the All Report view (?)
+   */
+  get allControls(): Control[] {
+    return this.allProfiles.flatMap(profile => profile.controls);
   }
 
   /**
-   * Adds a profile and all of it's controls to the store.
+   * Adds a profile file to the store.
    * @param newProfile The profile to add
    */
   @Mutation
-  addProfile(newProfile: Profile) {
-    this.allProfiles.push(newProfile);
-    this.allControls.push(...newProfile.controls);
+  addProfile(newProfile: ProfileFile) {
+    this.profileFiles.push(newProfile);
   }
 
   /**
-   * Adds a report, all of its profiles, and all of those profiles
-   * controls to the store.
+   * Adds a report file to the store   
    * @param newReport The report to add
    */
   @Mutation
-  addReport(newReport: InspecOutput) {
-    this.allReports.push(newReport);
-    newReport.profiles.forEach(profile => {
-      this.allProfiles.push(profile);
-      this.allControls.push(...profile.controls);
-    });
+  addReport(newReport: ReportFile) {
+    this.reportFiles.push(newReport);
   }
 
   /**
@@ -56,9 +93,8 @@ class InspecDataModule extends VuexModule {
    */
   @Mutation
   reset() {
-    this.allControls = [];
-    this.allProfiles = [];
-    this.allReports = [];
+    this.profileFiles = [];
+    this.reportFiles = [];
   }
 }
 
--- src/store/report_intake.ts
+++ src/store/report_intake.ts
@@ -7,38 +7,66 @@ import { Module, VuexModule, getModule, Action } from "vuex-module-decorators";
 import DataModule from "./data_store";
 import Store from "./store";
 
+// To give files unique id's with
+let file_index : number = 0;
+function genFileIndex(): number {
+  file_index++;
+  return file_index;
+}
+
 @Module({
   namespaced: true,
   // name: "intake",
 })
 class InspecIntakeModule extends VuexModule {
-  @Action
-  loadReportFile(reportText: string) {
-    // Parse the given text. TODO: Error checking
-
-    // Parse from json
-    let content = JSON.parse(reportText);
-
-    // Construct a report
-    let report = new InspecOutput(content);
-
-    // Deliver
-    let data = getModule(DataModule, Store);
-    data.addReport(report);
-  }
 
   @Action
-  loadProfileFile(profileText: string) {
-    // Parse the given text as a profile, and deliver it to the store. TODO: Error checking
-    // Parse from json
-    var content = JSON.parse(profileText);
+  async loadFile(file: File) {
+    // Make the reader
+    let reader = new FileReader();
 
-    // Construct a profile
-    var profile = new Profile(null, content);
+    // Setup the callback
+    reader.onload = (event: ProgressEvent) => {
+      // Get our text
+      let text = reader.result as string;
 
-    // Deliver
-    let data = getModule(DataModule, Store);
-    data.addProfile(profile);
+      // Parse as json
+      let json = JSON.parse(text);
+
+      // TODO: Verify that no errors occurred
+
+      // Fetch our data store
+      let data = getModule(DataModule, Store);
+
+      // Retrieve common elements for either case (profile or report)
+      let unique_id = genFileIndex();
+      let filename = file.name;
+
+      // Determine what sort of file we (hopefully) have, then add it
+      if("profiles" in json) {
+        // It must be a report
+        let report = new InspecOutput(json);
+        let reportFile = {
+          unique_id,
+          filename,
+          report,
+        }
+        data.addReport(reportFile);
+      } else {
+        // It must be a profile
+        let profile = new Profile(null, json);
+        let profileFile = {
+          unique_id,
+          filename,
+          profile
+        }
+        data.addProfile(profileFile);
+      }
+
+    }
+
+    // Dispatch the read
+    reader.readAsText(file);
   }
 }
 
