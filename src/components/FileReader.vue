024ae635af64cedea86485b126f3434076eef092
Henry <jchenry@mm249849-pc.mitre.org>
2019-07-17 18:19:02 -0400

    MAINT: Split filereader behaviour to modules

--- src/components/FileReader.vue
+++ src/components/FileReader.vue
@@ -42,74 +42,10 @@ export default {
   },
   methods: {
     loadTextFromFile(ev) {
-      const file = ev.target.files[0];
-      const reader = new FileReader();
-      const store = this.$store;
-      const routes = router.routes;
-      const id = Items[1].submenu.length;
-      let fileName = file.name;
-
-      let fileCount = 2;
-
-      for (let results in Items[1].submenu) {
-        if (Items[1].submenu[results].name == fileName) {
-          if (fileCount < 3) {
-            fileName = fileName + ' ' + fileCount;
-            fileCount++;
-          }
-          else {
-            fileName = fileName.substr(0, fileName.length - 2) + ' ' + fileCount;
-            fileCount++;
-          }
-        }
-      }
-
-      reader.onload = function(){
-        var text = reader.result;
-        store.commit("data/reset"); 
-        // store.setTitle(file.name);
-        store.dispatch("intake/loadReportFile", text);
-        // store.parseFile(text, file.name);
-      };
-      reader.readAsText(file);
-
-      //ADDS SIDE BAR ITEM
-      Items[1].submenu.push({
-        url: "/results/" + id, 
-        name: fileName,
-        slug: "result" + id,
-        icon: "FileIcon",
-      })
-
-      router.push("/results/" + id)
-    },
-    getTitle() {
-      return "TODO!";
+      file = ev.target.files[0];
+      store.commit("intake/loadFile", file);
     },
   }
 };
 
 </script>
-<style>
-/*
-input[type="file" i] {
-    -webkit-appearance: initial;
-    background-color: #FF00FF !important;
-    color: white;
-    cursor: default;
-    padding: initial;
-    border: initial;
-}
-.text-reader {
-    font-size: 1.25em;
-    font-weight: 700;
-    color: white;
-    background-color: #6c757d;
-    display: inline-block;
-}
-
-.text-reader:focus,
-.text-reader:hover {
-    background-color: red;
-}*/
-</style>
